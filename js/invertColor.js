cde7c28b5a9200484afdf44beb3c17a28fc5c3ee
Felipe Alzamora <luisfalmi@gmail.com>
2021-03-07 01:41:49 -0300

    fix problem gray background and text

--- js/invertColor.js
+++ js/invertColor.js
@@ -1,9 +1,18 @@
 "use strict";
 
 function invertColor(rgb) {
-    var inv_rgb = [];
-    for (var i = 0; i < rgb.length; i++) {
+    let inv_rgb = [];
+    let actualMid = 0;
+
+    for (let i = 0; i < rgb.length; i++) {
+      
+      if((+rgb[i] >= 116) && (+rgb[i] <= 140)){
+        actualMid++;
+      }
       inv_rgb.push(255-rgb[i]);
+    }
+    if(actualMid === rgb.length){
+        return [0,0,0];
     }
     return inv_rgb;
 }
